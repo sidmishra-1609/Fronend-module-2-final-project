9ac04db3345bc88e45fa428a115d09c9ad4e6f68
Ritika Mishra <sidmishra1609@gmail.com>
2023-04-04 22:26:55 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,172 @@
+Object
+date
+: 
+"2023-04-04"
+explanation
+: 
+"The largest volcano in our Solar System is on Mars.  Although three times higher than Earth's Mount Everest, Olympus Mons will not be difficult for humans to climb because of the volcano's shallow slopes and Mars' low gravity.  Covering an area greater than the entire Hawaiian volcano chain, the slopes of Olympus Mons typically rise only a few degrees at a time.  Olympus Mons is an immense shield volcano, built long ago by fluid lava.  A relatively static surface crust allowed it to build up over time. Its last eruption is thought to have been about 25 million years ago.  The featured image was taken by the European Space Agency's robotic Mars Express spacecraft currently orbiting the  Red Planet.    Your Sky Surprise: What picture did APOD feature on your birthday? (post 1995)"
+hdurl
+: 
+"https://apod.nasa.gov/apod/image/2304/OlympusMons_MarsExpress_6000.jpg"
+media_type
+: 
+"image"
+service_version
+: 
+"v1"
+title
+: 
+"Olympus Mons: Largest Volcano in the Solar System"
+url
+: 
+"https://apod.nasa.gov/apod/image/2304/OlympusMons_MarsExpress_960.jpg"
+[[Prototype]]
+: 
+Object
+constructor
+: 
+ƒ Object()
+assign
+: 
+ƒ assign()
+create
+: 
+ƒ create()
+defineProperties
+: 
+ƒ defineProperties()
+defineProperty
+: 
+ƒ defineProperty()
+entries
+: 
+ƒ entries()
+freeze
+: 
+ƒ freeze()
+fromEntries
+: 
+ƒ fromEntries()
+getOwnPropertyDescriptor
+: 
+ƒ getOwnPropertyDescriptor()
+getOwnPropertyDescriptors
+: 
+ƒ getOwnPropertyDescriptors()
+getOwnPropertyNames
+: 
+ƒ getOwnPropertyNames()
+getOwnPropertySymbols
+: 
+ƒ getOwnPropertySymbols()
+getPrototypeOf
+: 
+ƒ getPrototypeOf()
+hasOwn
+: 
+ƒ hasOwn()
+is
+: 
+ƒ is()
+isExtensible
+: 
+ƒ isExtensible()
+isFrozen
+: 
+ƒ isFrozen()
+isSealed
+: 
+ƒ isSealed()
+keys
+: 
+ƒ keys()
+length
+: 
+1
+name
+: 
+"Object"
+preventExtensions
+: 
+ƒ preventExtensions()
+prototype
+: 
+{constructor: ƒ, __defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, __lookupGetter__: ƒ, …}
+seal
+: 
+ƒ seal()
+setPrototypeOf
+: 
+ƒ setPrototypeOf()
+values
+: 
+ƒ values()
+arguments
+: 
+(...)
+caller
+: 
+(...)
+[[Prototype]]
+: 
+ƒ ()
+[[Scopes]]
+: 
+Scopes[0]
+hasOwnProperty
+: 
+ƒ hasOwnProperty()
+isPrototypeOf
+: 
+ƒ isPrototypeOf()
+propertyIsEnumerable
+: 
+ƒ propertyIsEnumerable()
+toLocaleString
+: 
+ƒ toLocaleString()
+toString
+: 
+ƒ toString()
+valueOf
+: 
+ƒ valueOf()
+__defineGetter__
+: 
+ƒ __defineGetter__()
+__defineSetter__
+: 
+ƒ __defineSetter__()
+__lookupGetter__
+: 
+ƒ __lookupGetter__()
+__lookupSetter__
+: 
+ƒ __lookupSetter__()
+__proto__
+: 
+(...)
+get __proto__
+: 
+ƒ __proto__()
+set __proto__
+: 
+ƒ __proto__()
+length
+: 
+1
+name
+: 
+"set __proto__"
+arguments
+: 
+(...)
+caller
+: 
+(...)
+[[Prototype]]
+: 
+ƒ ()
+[[Scopes]]
+: 
+Scopes[0]
